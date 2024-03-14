ba5c0b54f459a6039817bb246089972eb683c22f
leonardo monteiro <leomonteirodos@gmail.com>
2024-03-14 15:04:22 -0300

    primriro commit

--- scripts.js
+++ scripts.js
@@ -19,35 +19,3 @@ document.getElementById('ticketForm').addEventListener('submit', function(event)
     document.getElementById('ticketForm').reset();
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("ticketForm").addEventListener("submit", function(event) {
-        event.preventDefault(); // Impede o envio padrão do formulário
-        
-        // Capturar os valores do formulário
-        var title = document.getElementById("title").value;
-        var category = document.getElementById("category").value;
-        var description = document.getElementById("description").value;
-
-        // Obter a hora atual
-        var data = new Date();
-        var hora = data.getHours();
-        var minutos = data.getMinutes();
-        var segundos = data.getSeconds();
-        var horaFormatada = hora + ":" + minutos + ":" + segundos;
-
-        // Montar o conteúdo do arquivo
-        var conteudoArquivo = "Nome do colaborador: " + title + "\n";
-        conteudoArquivo += "Categoria: " + category + "\n";
-        conteudoArquivo += "Descrição: " + description + "\n";
-        conteudoArquivo += "Hora do envio: " + horaFormatada;
-
-        // Criar o objeto Blob com o conteúdo do arquivo
-        var blob = new Blob([conteudoArquivo], { type: "text/plain" });
-
-        // Criar um elemento <a> para fazer o download do arquivo
-        var linkDownload = document.createElement("a");
-        linkDownload.download = "ticket_" + horaFormatada + ".txt";
-        linkDownload.href = URL.createObjectURL(blob);
-        linkDownload.click();
-    });
-});
